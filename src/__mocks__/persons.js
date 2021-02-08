a940e5a0dc284abe1490c2e7544a26cf382e3fbf
jozekcore <josedbarrios7@gmail.com>
2021-02-07 20:42:53 -0500

    Add firebase and connect data with rtdb

--- src/__mocks__/persons.js
+++ src/__mocks__/persons.js
@@ -1,6 +1,5 @@
 const persons = [
   {
-    id: 1,
     name: 'Pope Francis',
     description:
       "He's talking tough on clergy sexual abuse, but is he just another papal pervert protector? (thumbs down) or true pedophile punishing pontiff? (thumbs up)",
@@ -15,7 +14,6 @@ const persons = [
     category: 'Religion'
   },
   {
-    id: 2,
     name: 'Kanye West',
     image: '/images/kanye-west.png',
     votes: {
@@ -30,7 +28,6 @@ const persons = [
     category: 'Entertainment'
   },
   {
-    id: 3,
     name: 'Mark Zuckerberg',
     image: '/images/mark-zuckerberg.png',
     votes: {
@@ -45,7 +42,6 @@ const persons = [
     category: 'Business'
   },
   {
-    id: 4,
     name: 'Cristina Fernandez de Kirchner',
     image: '/images/cristina-kirchner.png',
     votes: {
@@ -60,7 +56,6 @@ const persons = [
     category: 'Politics'
   },
   {
-    id: 5,
     name: 'Malala Yousafzai',
     image: '/images/malala-yousafzai.png',
     votes: {
--- src/pages/[lng]/index.js
+++ src/pages/[lng]/index.js
@@ -1,24 +1,51 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
 import Hero from '~/components/Hero/Hero';
-import votedPersons from '~/__mocks__/persons';
+import firebase from '~/firebase/clientApp';
 import Layout from '~/components/Layout/Layout';
+import DottedLine from '~/components/DottedLine/DottedLine';
 import VoteSection from '~/components/VoteSection/VoteSection';
 import SmallBanner from '~/components/SmallBanner/SmallBanner';
-import DottedLine from '~/components/DottedLine/DottedLine';
 import BeHeardMessage from '~/components/BeHeardMessage/BeHeardMessage';
 
-const HomePage = ({ persons, mainPerson }) => (
-  <Layout>
-    <Hero person={mainPerson} />
-    <BeHeardMessage />
-    <VoteSection persons={persons} />
-    <SmallBanner />
-    <DottedLine />
-  </Layout>
-);
+const HomePage = ({ initialPersons, mainPerson }) => {
+  const [persons, setPersons] = useState(initialPersons);
 
-export async function getServerSideProps({ params }) {
+  useEffect(() => {
+    const ref = firebase.database().ref('persons');
+    ref.on('child_changed', (snapshot) => {
+      const personUpdated = snapshot.val();
+
+      const newPersons = persons.map((person) =>
+        personUpdated.id === person.id ? personUpdated : person
+      );
+
+      setPersons(newPersons);
+    });
+  }, [persons]);
+
+  return (
+    <Layout>
+      <Hero person={mainPerson} />
+      <BeHeardMessage />
+      <VoteSection persons={persons} />
+      <SmallBanner />
+      <DottedLine />
+    </Layout>
+  );
+};
+
+export const getServerSideProps = async ({ params }) => {
+  let votedPersons = [];
+  const ref = firebase.database().ref('persons');
+
+  // call voted persons from firebase
+  await ref.once('value', (snapshot) => {
+    votedPersons = snapshot.val();
+  });
+
+  // get locale i18 file from url
   const { default: lngDict = {} } = await import(
     `public/locales/${params.lng}.json`
   );
@@ -30,14 +57,16 @@ export async function getServerSideProps({ params }) {
       lngDict,
       mainPerson,
       lng: params.lng,
-      persons: votedPersons.filter((person) => person.id !== mainPerson.id)
+      initialPersons: votedPersons.filter(
+        (person) => person.id !== mainPerson.id
+      )
     }
   };
-}
+};
 
 HomePage.propTypes = {
   mainPerson: PropTypes.shape({}).isRequired,
-  persons: PropTypes.arrayOf(PropTypes.shape({})).isRequired
+  initialPersons: PropTypes.arrayOf(PropTypes.shape({})).isRequired
 };
 
 export default HomePage;
